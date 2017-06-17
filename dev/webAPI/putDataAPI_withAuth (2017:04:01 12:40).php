42ac86d8f42f4d797d761c84525db08729a85015
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-17 22:58:19 +0900

    added display column CPU temp

--- dev/webAPI/putDataAPI_withAuth (2017:04:01 12:40).php
+++ dev/webAPI/putDataAPI_withAuth (2017:04:01 12:40).php
@@ -6,7 +6,7 @@ if (!isset($_GET["c"])) {
     echo json_encode(array("ERROR" => "No authentication code."));
     exit();
 }
-$authCode = "TsaJt1fR%5SyN";//this is the key auth
+$authCode = "TsaJt1fR5SyN";//this is the key auth
 if (isset($_GET["c"]) && $_GET["c"] != $authCode) {
     echo json_encode(array("ERROR" => "Invalid authentication code."));
     exit();
@@ -21,15 +21,23 @@ $eValue = $_GET["lux"];
 $fValue = $_GET["temp"]; //data "temp" is cpu teperature
 $gValue = $_GET["v0"];
 $hValue = $_GET["v1"];
+$iValue = $_GET["memo"];
+$jValue = $_GET["deploy"]; //this is not a data for store database.
+//this is just used for switch deply sisitem and sandBox
  if ($aValue < 1) {
      echo json_encode(array("ERROR" => "Invalid Number."));
      exit();
 }
+if ($jValue == "sandBox") {
+	$tableName = "atmos_test";
+} else { #support regacy 
+	$tableName = "atmos";
+}
 $dbInstance = new DB_Proxy();
 $dbInstance->ignoringPost();
 $dbInstance->initialize(
-    array(array('name' => 'atmos', 'key' => 'id',),), 
-    array(), array("db-class" => "PDO"), 2, "atmos");
+    array(array('name' => $tableName, 'key' => 'id',),), 
+    array(), array("db-class" => "PDO"), 2, $tableName);
 $dbInstance->dbSettings->addValueWithField("date", $aValue);
 $dbInstance->dbSettings->addValueWithField("temp", $bValue);
 $dbInstance->dbSettings->addValueWithField("pressure", $cValue);
@@ -38,8 +46,10 @@ $dbInstance->dbSettings->addValueWithField("lux", $eValue);
 $dbInstance->dbSettings->addValueWithField("cpu", $fValue);
 $dbInstance->dbSettings->addValueWithField("v0", $gValue);
 $dbInstance->dbSettings->addValueWithField("v1", $hValue);
+$dbInstance->dbSettings->addValueWithField("memo", $iValue);
 $dbInstance->processingRequest("create");
 $pInfo = $dbInstance->getDatabaseResult();
 $logInfo = $dbInstance->logger->getMessagesForJS();
-echo json_encode(array("data"=>$pInfo,"log"=>$logInfo));
+//echo json_encode(array("data"=>$pInfo,"log"=>$logInfo));
+var_export($logInfo, false);
 ?>
