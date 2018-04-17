02c7a36f8240d7a3d84ed428bb42188d42929ed8
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-17 18:33:03 +0900

    echo( system('git pull', $ret) );

--- dev/gitPull.php
+++ dev/gitPull.php
@@ -5,8 +5,5 @@ var_dump( system('ls -la', $ret) );
 var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 var_dump( system('cd ~/www/IM', $ret) );
-echo('<pre>');
-var_dump( system('git pull', $ret) );
-var_dump( $ret );
-echo('<pre>');
+echo( system('git pull', $ret) );
 ?>
