8902b0ffb8747d29f1b0509682da60c02efb5479
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-17 23:01:29 +0900

    almoset done

--- dev/gitPull.php
+++ dev/gitPull.php
@@ -4,5 +4,4 @@ echo "\n# system関数\n";
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 var_dump( system('cd ~/www/IM/dev/', $ret));
 var_dump(system('git pull 2>&1', $ret));
-echo ${ret};
 ?>
