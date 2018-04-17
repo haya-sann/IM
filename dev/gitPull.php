e34123b22c5837fa855a02c2394f80afe3e1b1f6
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-17 23:08:55 +0900

    ユーザーへのアナウンス文言を変更

--- dev/gitPull.php
+++ dev/gitPull.php
@@ -1,5 +1,5 @@
 <?php
-echo "\n# system関数\n";
+echo "\n# プログラムのアップデータがあれば、更新します\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
