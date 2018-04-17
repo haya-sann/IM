74b5cc90cdf0ee0cb5dd176101f353ffc9e8f97a
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-17 23:06:38 +0900

    without var_dump system('cd ~/www/IM/dev/');

--- dev/gitPull.php
+++ dev/gitPull.php
@@ -3,6 +3,6 @@ echo "\n# system関数\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
-var_dump( system('cd ~/www/IM/dev/'));
+system('cd ~/www/IM/dev/');
 var_dump(system('git pull 2>&1', $ret));
 ?>
