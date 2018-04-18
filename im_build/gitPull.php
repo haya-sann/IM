086e98d1ef28a0f66a720b75431d5293ebd4391c
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-19 01:55:46 +0900

    set update directory

--- im_build/gitPull.php
+++ im_build/gitPull.php
@@ -3,6 +3,6 @@ echo "\n# プログラムのアップデータがあれば、更新します\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
-system('cd ~/www/IM/dev/');
+system('cd ~/www/IM/im_build/');
 system('git pull 2>&1');
 ?>
