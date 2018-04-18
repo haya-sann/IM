58786120e04303723949b36063c649637364631b
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-19 02:00:24 +0900

    test

--- im_build/gitPull.php
+++ im_build/gitPull.php
@@ -3,6 +3,6 @@ echo "\n# プログラムのアップデータがあれば、更新します\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
-system('cd ~/www/IM/im_build/');
+system('cd ~/www/IM/dev/');
 system('git pull 2>&1');
 ?>
