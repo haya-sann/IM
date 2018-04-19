0cc63a1567aca9953f696f98f4272e5ee1adf1a3
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-19 21:28:57 +0900

    update dev directory

--- dev/gitPull.php
+++ dev/gitPull.php
@@ -1,5 +1,5 @@
 <?php
-echo "\n# プログラムのアップデータがあれば、更新します\n";
+echo "\n# プログラムの新規アップデータを探します。\n新しいものがあれば、差し替えます\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
