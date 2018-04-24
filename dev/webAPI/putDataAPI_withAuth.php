56a751ef4688a8e96aef2a94c614a32bed66a1d3
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-24 22:04:13 +0900

    forgot \n

--- dev/webAPI/putDataAPI_withAuth.php
+++ dev/webAPI/putDataAPI_withAuth.php
@@ -40,7 +40,7 @@ $datetime1 = new DateTime($prevTimeValue);
 $datetime2 = new DateTime($accessTime);
 $diffTime = $datetime1->diff($datetime2);
 
-$lValue .= "\前回のアクセス記録：".$prevTimeValue ."　　今回のアクセス時刻：".$accessTime;
+$lValue .= "\n前回のアクセス記録：".$prevTimeValue ."　　今回のアクセス時刻：".$accessTime;
 $lValue .= "\n前回アクセスからの経過時間：".$diffTime->format('%H:%I:%S');
 
 if ($accessTime < 1) {
