de0439e2aa68181ae19039a97694450c2a32d75f
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-08 23:31:45 +0900

    just update test

--- gitPull.php
+++ gitPull.php
@@ -1,5 +1,5 @@
 <?php
-echo "\n# プログラムの新規アップデータを探します。\n新しいものがあれば、差し替えます\n";
+echo "\n# プログラムの新規アップデータを探して、\n新しいものがあれば、差し替えます\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
