bbc61386728f5ac4f2bf6ef5bcf5b8542775be46
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-09 00:12:19 +0900

    backup gitPull.php

--- bak/gitPull.php
+++ bak/gitPull.php
@@ -1,8 +1,8 @@
 <?php
-echo "\n# プログラムの新規アップデータを探します。\n新しいものがあれば、差し替えます\n";
+echo "\n# プログラムの新規アップデータを探して、\n新しいものがあれば、差し替えます\n";
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
-system('cd ~/www/IM/');
+system('cd /IM/');
 system('git pull 2>&1');
 ?>
