96055a02d5f0590b9934ffae0144c52b408173eb
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 01:23:45 +0900

    print '以下の通り，更新しました';

--- gitPull.php
+++ gitPull.php
@@ -9,6 +9,7 @@ system('git pull', $response);
 if (preg_match('Already up-to-date.',$response)) { 
     print '調べましたが，新しいアップデータはありません';
 } else {
+    print '以下の通り，更新しました';
     var_dump($response);//実行結果葉全部出す？
 }
 ?>
