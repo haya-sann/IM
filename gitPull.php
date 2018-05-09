55e24b33cee92258f1bd9de21ce7006c6f7f51b6
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 00:03:49 +0900

    added comment for IM　directoy

--- gitPull.php
+++ gitPull.php
@@ -3,6 +3,6 @@ echo "\n# プログラムの新規アップデータを探して、\n新しい�
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
-system('cd /IM/');
+system('cd /IM/');//サイトのWeb rootに置いたIMディレクトリーに移動する。
 system('git pull 2>&1');
 ?>
