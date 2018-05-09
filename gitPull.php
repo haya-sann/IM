50d4a6806c8a7baad2a53274f4ab897ca027a330
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 01:33:58 +0900

    using exec

--- gitPull.php
+++ gitPull.php
@@ -3,9 +3,9 @@ echo "\n# プログラムの新規アップデータを探して、\n新しい�
 //var_dump( system('eval "$(ssh-agent -s)"', $ret) );
 //var_dump( system('ssh-add ~/.ssh/id_rsa_github', $ret) );
 
-system('cd /IM/');//サイトのWeb rootに置いたIMディレクトリーに移動する。
+exec('cd /IM/');//サイトのWeb rootに置いたIMディレクトリーに移動する。
 //system('git pull 2>&1');
-system('git pull', $response);
+exec('git pull', $response);
 if (preg_match('Already up-to-date.',$response)) { //正規表現にした
     print '調べましたが，新しいアップデータはありません';
 } else {
