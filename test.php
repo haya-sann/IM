fbdc3e9bab00ecac273c0736dc1fe068163eb489
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 15:05:33 +0900

    $checkSrting

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ exec($cmd, $arr, $res);
 var_dump($arr); 
 var_dump($res); 
 $checkSrting = "Already up to date.";
+var_dump($checkSrting);
 $res_2=in_array($checkString,$arr,true); //第3因数にtrueを渡さないとうまく比較できない
 var_dump($res_2);
 ?>
