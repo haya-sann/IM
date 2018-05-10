103e1ea1735899bcfce5e1facf7d7ee893e0e931
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 14:47:52 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ $cmd = "git pull";
 exec($cmd, $arr, $res); 
 var_dump($arr); 
 var_dump($res); 
-$res_2=in_array('Already',$arr); 
+$res_2=in_array('Already',$arr); //なぜ，検出されない？
 var_dump($res_2);
 ?>
 
