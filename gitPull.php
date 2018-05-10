b3b2f2f22114e5d201e7d7edbbe820578a14b445
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 22:00:25 +0900

    echo "\n更新しました\n";

--- gitPull.php
+++ gitPull.php
@@ -7,14 +7,14 @@ exec('cd /IM/');//サイトのWeb rootに置いたIMディレクトリーに移�
 exec('ls');
 //system('git pull 2>&1');
 exec('git pull', $result, $response);
-$strResult = implode ( $result );
+//$strResult = implode ( $result );
 if (in_array("Already up-to-date.", $result, true)) { //正規表現にした
-    echo '<br>現在既に最新の状態です';
+    echo "\n現在既に最新の状態です";
 } else {
-    echo "\n下記のように，更新しました\n";//改行を出力するには""で囲む必要がある。'はだめ
+    echo "\n更新しました\n";//改行を出力するには""で囲む必要がある。'はだめ
     var_dump ($result);
-    print "変数\$resultは：";
-    print_r ($strResult);
-    print_r ($response);
+//    print "変数\$resultは：";
+//    print_r ($strResult);
+//    print_r ($response);
 }
 ?>
