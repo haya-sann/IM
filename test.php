6f8491e4cc1df880008c3d698587998f7cc4158f
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 15:03:29 +0900

    forgot 2nd , third parameter

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ exec($cmd, $arr, $res);
 var_dump($arr); 
 var_dump($res); 
 $checkSrting = "Already up to date.";
-$res_2=in_array($checkString; //第3因数にtrueを渡さないとうまく比較できない
+$res_2=in_array($checkString,$arr,true); //第3因数にtrueを渡さないとうまく比較できない
 var_dump($res_2);
 ?>
 
