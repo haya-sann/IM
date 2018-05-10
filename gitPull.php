80c8bd6db6defa03103ae4a59bb6e2ddba3cf362
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 22:49:58 +0900

    print_r ($result);

--- gitPull.php
+++ gitPull.php
@@ -11,6 +11,6 @@ if (in_array("Already up-to-date.", $result, true)) { //正規表現にした
     echo "\n現在既に最新の状態です";
 } else {
     echo "\n下記のように更新しました\n";//改行を出力するには""で囲む必要がある。'はだめ
-    print_r ($result); //print
+    print_r ($result); //print_rで行列を表示
 }
 ?>
