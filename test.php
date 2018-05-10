faa94487497f32287263c6e40b3585c002bd7fb1
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 15:00:42 +0900

    $checkSrting = "Already up to date.";
    added definition $checkSrting = "Already up to date.";

--- test.php
+++ test.php
@@ -3,7 +3,8 @@ $cmd = "git pull";
 exec($cmd, $arr, $res); 
 var_dump($arr); 
 var_dump($res); 
-$res_2=in_array('Already',$arr,true); //第3因数にtrueを渡さないとうまく比較できない
+$checkSrting = "Already up to date.";
+$res_2=in_array($checkString; //第3因数にtrueを渡さないとうまく比較できない
 var_dump($res_2);
 ?>
 
