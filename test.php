84f22eadfb98f367cf9f51a6bf34b75087632cad
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 14:55:40 +0900

    in_array('Already',$arr,true);
    in_array('Already',$arr,true); //第3因数にtrueを渡さないとうまく比較できない

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ $cmd = "git pull";
 exec($cmd, $arr, $res); 
 var_dump($arr); 
 var_dump($res); 
-$res_2=in_array('Already',$arr); //なぜ，検出されない？
+$res_2=in_array('Already',$arr,true); //第3因数にtrueを渡さないとうまく比較できない
 var_dump($res_2);
 ?>
 
