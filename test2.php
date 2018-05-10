4bb37224717a5d8d3c9bd70a182bb66eb40505f3
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-10 20:48:57 +0900

    $res = in_array("Already", $os);

--- test2.php
+++ test2.php
@@ -1,10 +1,10 @@
 <?php
 $os = array("Mac", "NT", "Irix", "Linux", "Already up to date.");
 var_dump($os);
-$res = in_array("Already up to date.", $os);
+$res = in_array("Already", $os);
 var_dump($res);
 if (in_array("Irix", $os)) {
-    echo "Got Irix";
+    echo "Got Irix\n";
 }
 if (in_array("Already", $os)) {
     echo "最新のプログラムです";
