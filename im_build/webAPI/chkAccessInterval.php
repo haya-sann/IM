d2138677e31ef5d9a2d6dd0b609b7f5349658936
Nobuo Hayashi <haya.biz@gmail.com>
2021-08-29 17:38:41 +0900

    時間差

--- im_build/webAPI/chkAccessInterval.php
+++ im_build/webAPI/chkAccessInterval.php
@@ -4,4 +4,5 @@ echo "Today is " . date("Y.m.d") . "\n";
 echo "Today is " . date("Y-m-d") . "\n";
 echo "Today is " . date("l") . "\n";
 echo "The time is " . date("h:i:sa") . "\n";
+echo "前回アクセスからの時間差："
 ?>
