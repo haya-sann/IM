fc132182560d248ad97b6c146c91cb0bf4736802
Nobuo Hayashi <haya.biz@gmail.com>
2021-08-29 17:42:27 +0900

    前回アクセス時間表示

--- im_build/webAPI/chkAccessInterval.php
+++ im_build/webAPI/chkAccessInterval.php
@@ -4,5 +4,6 @@ echo "Today is " . date("Y.m.d") . "\n";
 echo "Today is " . date("Y-m-d") . "\n";
 echo "Today is " . date("l") . "\n";
 echo "The time is " . date("h:i:sa") . "\n";
-echo "前回アクセスからの時間差："
+$d=strtotime("10:30pm April 15 2014");
+echo "前回アクセスの時間： " . date("Y-m-d h:i:sa", $d);
 ?>
