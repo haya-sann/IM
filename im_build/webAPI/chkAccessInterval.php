71e2121007554c14def80484e86e3060efe7fbbb
Nobuo Hayashi <haya.biz@gmail.com>
2021-08-29 17:35:41 +0900

    time is

--- im_build/webAPI/chkAccessInterval.php
+++ im_build/webAPI/chkAccessInterval.php
@@ -1,6 +1,7 @@
 <?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("Y.m.d") . "<br>";
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+echo "Today is " . date("Y/m/d") . "\n";
+echo "Today is " . date("Y.m.d") . "\n";
+echo "Today is " . date("Y-m-d") . "\n";
+echo "Today is " . date("l") . "\n";
+echo "The time is " . date("h:i:sa");
 ?>
