df3ebde21f3c0657976e062cf66fb06aed7a3910
Nobuo Hayashi <haya.biz@gmail.com>
2021-08-29 18:10:32 +0900

    $TimeRug

--- im_build/webAPI/chkAccessInterval.php
+++ im_build/webAPI/chkAccessInterval.php
@@ -8,8 +8,9 @@ echo "The time is " . date("h:i:sa") . "\n";
 $myfile = fopen("prevTime.txt", "r") or die("Unable to open file!");
 $prevAccessTime =  fgets($myfile);
 fclose($myfile);
+echo "前回アクセス時刻：" . $prevAccessTime . "\n";
 
-$TimeRug = strtotime(date("h:i:sa") - $prevAccessTime );
+$TimeRug = strtotime(date("Y-m-d h:i:sa") - $prevAccessTime );
 
 echo "前回アクセスからの時間差： " . date("Y-m-d h:i:sa", $TimeRug) . "\n";
 ?>
