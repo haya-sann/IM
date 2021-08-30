34fb9b0b590a1b403baa81e25297886eaf5f7b15
Nobuo Hayashi <haya.biz@gmail.com>
2021-08-30 15:56:01 +0900

    testSendMail

--- im_build/webAPI/chkAccessInterval.php
+++ im_build/webAPI/chkAccessInterval.php
@@ -1,15 +1,23 @@
 <?php
-echo "Today is " . date("Y/m/d") . "\n";
-echo "Today is " . date("Y.m.d") . "\n";
-echo "Today is " . date("Y-m-d") . "\n";
-echo "Today is " . date("l") . "\n";
-echo "The time is " . date("h:i:sa") . "\n";
+#https://www.w3schools.com/php/func_date_microtime.asp
+#にライブ解説がある。
+date_default_timezone_set("Asia/Tokyo");
+
 #ファイルから前回アクセス時刻を読み取る
 $myfile = fopen("prevTime.txt", "r") or die("Unable to open file!");
 $prevAccessTime =  fgets($myfile);
 fclose($myfile);
-echo "前回アクセス時刻：" . $prevAccessTime . "\n";
-$time_Now = date("h:i:sa");
-$TimeRug = date_diff($prevAccessTime, $time_Now);
-echo "前回アクセスからの時間差： " . date_format($TimeRug, "H:i:s") . "\n";
+echo "前回アクセス時刻：" . $prevAccessTime . "<br>";
+$previousTime = date_create($prevAccessTime);
+$time_Now = date_create("now");
+echo "現在時刻＝" . date_format ($time_Now, "H:i:s");
+$delayedTime = date_diff($previousTime,  $time_Now);
+
+echo "　　前回アクセスからの時間差： " . $delayedTime->format('%H:%i:%s') . "<br>";
+$NoAccessPeriod = ($delayedTime->format("%h"))*60 + $delayedTime->format("%i");
+
+if ($NoAccessPeriod > 60 ) {
+    echo "１時間以上アクセスがありません:". $NoAccessPeriod . "分" ;
+    
+}
 ?>
