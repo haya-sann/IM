329795d3ac8bd9329f3162e63995f9920035db23
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-06 02:34:14 +0900

    connect sample

---
+++ connect_db.php
@@ -0,0 +1,16 @@
+<?php
+try{
+    $conf = include ('defAtmos.php');
+    $DB_DATABASE = 'LAA0710594-satoyama';
+    $DB_USERNAME = $conf['user'];
+    $DB_PASSWORD = $conf['password'];
+    $DB_OPTION = 'charset=utf8';
+    $PDO_DSN = $conf['dsn'];
+    $db = new PDO($PDO_DSN, $DB_USERNAME, $DB_PASSWORD,
+ [   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+ ]);
+ echo 'DB接続成功';
+ } catch(PDOException $e){
+ echo 'DB接続失敗';
+}
+?>
