4ff914d0d5c707dc2790667c65f63b3be9049af8
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-06 23:04:02 +0900

    入力しない

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 try {
   $db = new PDO('mysql:dbname=LAA0710594-satoyama;host=mysql116.phy.lolipop.lan;charset=utf8','LAA0710594','GzM6GnGk');
   // 入力した値をデータベースへ登録
-  $count = $db->exec('INSERT INTO 部品登録 SET 部品名="'. $_POST['buhin'] .'",登録日 = NOW()');
+  //$count = $db->exec('INSERT INTO 部品登録 SET 部品名="'. $_POST['buhin'] .'",登録日 = NOW()');
   // 登録した件数を表示
   echo $count . "件のデータを登録しました！";
 } catch (PDOException $e) {
