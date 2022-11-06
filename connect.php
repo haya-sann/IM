51e47f52ebbb0165a33115d57cf7bce7d4234015
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-06 23:07:52 +0900

    echo 'つながった？'

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
 // データーベースtestへ接続
+$count = 126
 try {
   $db = new PDO('mysql:dbname=LAA0710594-satoyama;host=mysql116.phy.lolipop.lan;charset=utf8','LAA0710594','GzM6GnGk');
   // 入力した値をデータベースへ登録
@@ -10,4 +11,5 @@ try {
   // 接続できなかったらエラー表示
   echo 'DB接続エラー！: ' . $e->getMessage();
 }
+echo 'つながった？？'
 ?>
