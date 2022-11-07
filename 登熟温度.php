39992b8de164c6e5d20ab2082a164adc72d31372
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-07 14:54:16 +0900

    print $sql + "\n";

--- 登熟温度.php
+++ 登熟温度.php
@@ -12,6 +12,7 @@ $data = "atmos";
 $new_table = "average_temp";
 // 対象テーブルを選択しSELECT文を変数tableへ格納
 $sql = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
+print $sql + "\n";
 // queryを実行し、結果を変数に格納
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
