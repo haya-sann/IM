6ec3eb4259b5f22f9f9fa53f458b31409ebe9ca4
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-07 14:48:45 +0900

    $sql, $stmt

--- 登熟温度.php
+++ 登熟温度.php
@@ -11,9 +11,9 @@ try {
 $data = "atmos";
 $new_table = "average_temp";
 // 対象テーブルを選択しSELECT文を変数tableへ格納
-$table = "CREATE TEMPORARY TABLE average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM average_temp;";
+$sql = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 // queryを実行し、結果を変数に格納
-$sql = $pdo->query($table);
+$stmt = $pdo->query($sql);
 // ヘッダー行
 echo '<tr>';
 echo '<td>' . '日付', '</td>';
@@ -22,7 +22,7 @@ echo '<td>' . '登熟温度', '</td>';
 echo '</tr>';
 echo "\n";
 // foreach文で繰り返し配列の中身を一行ずつ出力
-foreach ($sql as $row) {
+foreach ($stmt as $row) {
 // データベースのフィールド名で出力
   // echo $new_table . "のデータ：" . $row['日付'] . 'と' . $row['登熟温度'].'です'.'<br>';
   echo '<tr>';
