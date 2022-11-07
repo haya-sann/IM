8bee20852077fccff7bbc53726ddf6534ba5541c
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-07 14:11:39 +0900

    ATTR_EMULATE_PREPARES

--- connect.php
+++ connect.php
@@ -2,6 +2,9 @@
 // データーベースへ接続
 try {
   $db = new PDO('mysql:dbname=LAA0710594-satoyama_test;host=mysql116.phy.lolipop.lan;charset=utf8','LAA0710594','GzM6GnGk');
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       
   echo "データベース接続しました！";
 } catch (PDOException $e) {
   // 接続できなかったらエラー表示
