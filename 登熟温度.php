1e430cbf4f6298ba7159fe63c3f5da8ee8252c69
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-07 16:16:14 +0900

    <br>

--- 登熟温度.php
+++ 登熟温度.php
@@ -8,7 +8,7 @@ $new_table = "average_temp";
 // 対象テーブルを選択しSELECT文を変数tableへ格納
 $sql_test = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 $sql = "select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
-echo $sql . "\n";  //just for debug
+echo $sql . "<br>";  //just for debug
 // queryを実行し、結果を変数に格納
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
@@ -18,7 +18,7 @@ echo '<td>' . '日付', '</td>';
 echo '<td>' . '平均気温', '</td>';
 // echo '<td>' . '登熟温度', '</td>';
 echo '</tr>';
-echo "\n";
+echo "<br>";
 // foreach文で繰り返し配列の中身を一行ずつ出力
 foreach ($stmt as $row) {
 // データベースのフィールド名で出力
