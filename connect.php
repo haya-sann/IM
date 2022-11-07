f1aca78305f954fe4f15b6e09e7a1f61af7ffc10
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-07 14:04:09 +0900

    test

--- connect.php
+++ connect.php
@@ -1,9 +1,8 @@
 <?php
 // データーベースへ接続
-$count = 126;
 try {
-  $db = new PDO('mysql:dbname=LAA0710594-satoyama;host=mysql116.phy.lolipop.lan;charset=utf8','LAA0710594','GzM6GnGk');
-  echo $count . "データベース接続しました！";
+  $db = new PDO('mysql:dbname=LAA0710594-satoyama_test;host=mysql116.phy.lolipop.lan;charset=utf8','LAA0710594','GzM6GnGk');
+  echo "データベース接続しました！";
 } catch (PDOException $e) {
   // 接続できなかったらエラー表示
   echo 'DB接続エラー！: ' . $e->getMessage();
