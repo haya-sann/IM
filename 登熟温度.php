a32157caf5dd7b82198a1736a8b558fd67b87413
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-08 16:07:01 +0900

    １行にまとめた

--- 登熟温度.php
+++ 登熟温度.php
@@ -8,13 +8,16 @@ require_once('connect.php');
 $data = "atmos";
 $new_table = "average_temp";
 // 対象テーブルを選択しSELECT文を変数tableへ格納
+$sql = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date)); set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 $sql_1 = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_2 = "set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
-echo $sql_1 . "<br>";  //just for debug
-echo $sql_2 . "<br>";  //just for debug
+echo $sql . "<br>";  //just for debug
+// echo $sql_1 . "<br>";  //just for debug
+// echo $sql_2 . "<br>";  //just for debug
 // queryを実行し、結果を変数に格納
-$stmt = $pdo->query($sql_1);
-$stmt2 = $pdo->query($sql_2);
+$stmt = $pdo->query($sql);
+// $stmt = $pdo->query($sql_1);
+// $stmt2 = $pdo->query($sql_2);
 
 // ヘッダー行
 echo '<table>';
@@ -25,7 +28,7 @@ echo '<td>' . '登熟温度', '</td>';
 echo '</tr>';
 
 // foreach文で繰り返し配列の中身を一行ずつ出力
-foreach ($stmt2 as $row) {
+foreach ($stmt as $row) {
 // データベースのフィールド名で出力
   // echo $new_table . "のデータ：" . $row['日付'] . 'と' . $row['登熟温度'].'です'.'<br>';
   echo '<tr><td>' . $row ['日付'] . '</td>';
