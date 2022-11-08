a69c5e301e8b5612e76c4041774243ee936c8d27
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-08 20:56:12 +0900

    $sql_view1

--- 登熟温度.php
+++ 登熟温度.php
@@ -8,9 +8,9 @@ echo "<br>";
 $data = "atmos";
 $new_table = "average_temp";
 // 対象テーブルを選択しSELECT文を変数tableへ格納
-$sql_vew1 = "CREATE OR REPLACE VIEW $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
+$sql_view1 = "CREATE OR REPLACE VIEW $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_view2 = "CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM $new_table;";
-$sql_veiew3 = "select * from amount_temp;";
+$sql_view3 = "select * from amount_temp;";
 // $sql_tempTable = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date)); set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 $sql_1 = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_2 = "set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
