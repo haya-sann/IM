5b2b70fbd7f89b4552e49fb86648ed895653ab4d
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-08 21:00:17 +0900

    typo

--- 登熟温度.php
+++ 登熟温度.php
@@ -15,8 +15,8 @@ $sql_view3 = "select * from amount_temp;";
 $sql_1 = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_2 = "set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 echo "SQL Statement:" . $sql_view1 . "<br>";  //just for debug
-echo $sql_view1 . "<br>";  //just for debug
 echo $sql_view2 . "<br>";  //just for debug
+echo $sql_view3 . "<br>";  //just for debug
 // queryを実行し、結果を変数に格納
 $stmt_view1 = $pdo->query($sql_view1);
 $stmt_view2 = $pdo->query($sql_view2);
