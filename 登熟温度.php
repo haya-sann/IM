9f00f7cd75295b66feaceb056df3ef30b6890d84
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-09 12:13:44 +0900

    show tables;

--- 登熟温度.php
+++ 登熟温度.php
@@ -8,7 +8,7 @@ echo "<br>";
 // 対象テーブルを選択しSELECT文を変数tableへ格納
 $sql_view1 = "CREATE OR REPLACE VIEW average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_view2 = "CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM average_temp;";
-$sql_view3 = "select * from amount_temp;";
+$sql_view3 = "select * from amount_temp;
 // $sql_tempTable = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date)); set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 $sql_1 = "CREATE TEMPORARY TABLE average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_2 = "set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM average_temp;";
@@ -22,7 +22,9 @@ echo $sql_view3 . "<br>";  //just for debug
 // $stmt_view1 -> execute();
 // $stmt_view2 = $pdo->prepare($sql_view2);
 // $stmt_view2 -> execute();
-$stmt_view3 = $pdo->prepare($sql_view3);
+// $stmt_view3 = $pdo->prepare($sql_view3);
+// $stmt_view3 -> execute();
+$stmt_view3 = $pdo->prepare("show tables;");
 $stmt_view3 -> execute();
 // ヘッダー行
 echo '<table>';
@@ -36,9 +38,11 @@ echo '</tr>';
 foreach ($stmt_view3 as $row) {
 // データベースのフィールド名で出力
   // echo $new_table . "のデータ：" . $row['日付'] . 'と' . $row['登熟温度'].'です'.'<br>';
-  echo '<tr><td>' . $row ['日付'] . '</td>';
-  echo '<td>' . $row ['平均気温'] . '</td>';
-  echo '<td>' . $row ['登熟温度']. '</td>';
+  echo '<tr>';
+  echo '<td>' . $row. '</td>';
+  // echo '<td>' . $row ['日付'] . '</td>';
+  // echo '<td>' . $row ['平均気温'] . '</td>';
+  // echo '<td>' . $row ['登熟温度']. '</td>';
   echo '</tr>';
 }
 echo '</table>';
