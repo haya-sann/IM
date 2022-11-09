7dc433a7086ee3ebefd3cf7815dfc4b827862cbb
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-09 13:37:39 +0900

    view4

--- 登熟温度.php
+++ 登熟温度.php
@@ -6,14 +6,15 @@ require_once('connect.php');
 echo "<br>";
 
 // 対象テーブルを選択しSELECT文を変数tableへ格納
-$sql_view1 = "CREATE OR REPLACE VIEW average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
-$sql_view2 = "CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM average_temp;";
-$sql_view3 = "select * from amount_temp";
-$sql_view4 = "select sum(平均気温) as 登熟温度 FROM average_temp;";
+$sql_view1 = "CREATE OR REPLACE VIEW average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));"
+$sql_view2 = "CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM average_temp;"
+$sql_view3 = "select * from amount_temp;"
+$sql_view4 = "select sum(平均気温) as 登熟温度 FROM average_temp;"
 // $sql_tempTable = "CREATE TEMPORARY TABLE $new_table AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from $data where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date)); set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM $new_table;";
 $sql_1 = "CREATE TEMPORARY TABLE average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
-$sql_2 = "set @amount_temp=0.0; SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM average_temp;";
-$sql_test2 = "CREATE OR REPLACE VIEW average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM average_temp; select * from amount_temp;";
+$sql_2 = "set @amount_temp=0.0; CREATE OR REPLACE VIEW amount_temp as SELECT 日付, 平均気温, format(@amount_temp := @amount_temp + 平均気温, 6) as 登熟温度 FROM average_temp;";
+$sql_test2 = "CREATE OR REPLACE VIEW average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));"
+$sql_test3 = "CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM average_temp; select * from amount_temp;"
 $sql_test = "select date 日付, temp 平均気温, lux 登熟温度 from atmos limit 5;";
 echo "SQL Statement:" . $sql_view1 . "<br>";  //just for debug
 echo $sql_view2 . "<br>";  //just for debug
