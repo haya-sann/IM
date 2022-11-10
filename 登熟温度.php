dfd95d73337f8ed3f12bc581b4c7bef119e26b98
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-10 12:56:58 +0900

    $sql_final1

--- 登熟温度.php
+++ 登熟温度.php
@@ -16,7 +16,10 @@ $sql_2 = "set @amount_temp=0.0; CREATE OR REPLACE VIEW amount_temp as SELECT 日
 $sql_test2 = "CREATE OR REPLACE VIEW average_temp AS select from_days(to_days(date)) 日付, avg(temp) 平均気温 from atmos where date between '2022-08-08' and '2022-09-15' group by from_days(to_days(date));";
 $sql_test3 = "CREATE OR REPLACE VIEW amount_temp AS select sum(平均気温) as 登熟温度 FROM average_temp; select * from amount_temp;";
 $sql_test = "select date 日付, temp 平均気温, lux 登熟温度 from atmos limit 5;";
-$sql_view5 = "SELECT from_days(to_days(日付)), 平均気温, sum(C) as 登熟温度 FROM before_temp where 日付 >= '2022-08-08' AND 日付 < '2022-09-15'  GROUP BY from_days(to_days(日付)) ;";
+$sql_view5 = "SELECT from_days(to_days(日付)), 平均気温, sum(C) as 登熟温度 FROM before_temp GROUP BY from_days(to_days(日付))  HAVING from_days(to_days(日付)) >= '2022-08-08' AND from_days(to_days(日付)) < '2022-09-15' ;";
+$sql_final1 = "CREATE OR REPLACE VIEW average_temp AS SELECT from_days(to_days(date)) 日付, avg(temp) 平均気温 FROM atmos GROUP BY from_days(to_days(date));
+CREATE OR REPLACE VIEW before_temp AS SELECT A.日付, A.平均気温, B.日付 C, B.平均気温 D FROM average_temp A, average_temp B WHERE B.日付 <= A.日付;
+SELECT 日付, 平均気温, sum(D) as 登熟温度 FROM before_temp WHERE 日付 >= '2022-08-08' AND C >= '2022-08-08' GROUP BY 日付;"
 echo "SQL Statement:" . $sql_view5 . "<br>";  //just for debug
 // echo $sql_view2 . "<br>";  //just for debug
 // echo $sql_view3 . "<br>";  //just for debug
@@ -27,8 +30,10 @@ echo "SQL Statement:" . $sql_view5 . "<br>";  //just for debug
 // $stmt_view2 -> execute();
 // $stmt_view3 = $pdo->prepare($sql_view3);
 // $stmt_view3 -> execute();
-$stmt_view5 = $pdo->prepare($sql_view5);
-$stmt_view5 -> execute();
+// $stmt_view5 = $pdo->prepare($sql_view5);
+// $stmt_view5 -> execute();
+$stmt_final1 = $pdo->prepare($sql_final1);
+$stmt_final1 -> execute();
 // ヘッダー行
 echo '<table>';
 echo '<tr>';
@@ -37,7 +42,7 @@ echo '<th>' . '平均気温', '</th>';
 echo '<td>' . '登熟温度', '</td>';
 echo '</tr>';
 // foreach文で繰り返し配列の中身を一行ずつ出力
-foreach ($stmt_view5 as $row) {
+foreach ($stmt_final1 as $row) {
 // データベースのフィールド名で出力
   echo '<tr>';
   echo '<td>' . $row ['日付'] . '</td>';
