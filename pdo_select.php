bd50ee39c85a5593ba0c067e6a81e87f52a52457
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-17 23:36:37 +0900

    first try pdostatement

---
+++ pdo_select.php
@@ -0,0 +1,24 @@
+<?php
+
+// 先ほど作成したPDOインスタンス作成をそのまま使用します
+require 'connect.php';
+
+// SQL文を準備します。「:id」がプレースホルダーです。
+// $sql = 'SELECT * FROM atmos limit 10';
+$sql = 'SELECT * FROM atmos WHERE id < :id';
+// PDOStatementクラスのインスタンスを生成します。
+$prepare = $dbh->prepare($sql);
+
+// PDO::PARAM_INTは、SQL INTEGER データ型を表します。
+// SQL文の「:id」を「3」に置き換えます。つまりはidが10より小さいレコードを取得します。
+$prepare->bindValue(':id', 10, PDO::PARAM_INT);
+
+// プリペアドステートメントを実行する
+$prepare->execute();
+
+// PDO::FETCH_ASSOCは、対応するカラム名にふられているものと同じキーを付けた 連想配列として取得します。
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+
+// 結果を出力
+var_dump($result);
+?>
