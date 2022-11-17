c1d2ae46454d675bfbd151f270fb0e031593f649
Nobuo Hayashi <haya.biz@gmail.com>
2022-11-18 00:04:38 +0900

    複文

--- pdo_select.php
+++ pdo_select.php
@@ -4,8 +4,11 @@
 require 'connect.php';
 
 // SQL文を準備します。「:id」がプレースホルダーです。
-// $sql = 'SELECT * FROM atmos limit 10';
-$sql = 'SELECT date, temp FROM atmos WHERE id > :id limit 10';
+$sql = 'CREATE OR REPLACE VIEW average_temp AS SELECT from_days(to_days(date)) 日付, avg(temp) 平均気温 FROM atmos GROUP BY from_days(to_days(date));
+CREATE OR REPLACE VIEW before_temp AS SELECT A.日付, A.平均気温, B.日付 C, B.平均気温 D FROM average_temp A, average_temp B WHERE B.日付 <= A.日付;
+CREATE OR REPLACE VIEW cumulative_temp AS SELECT 日付, 平均気温, sum(D) as 登熟温度 FROM before_temp WHERE 日付 >= (SELECT day_bloom FROM bloom LIMIT 1) AND C >= (SELECT day_bloom FROM bloom LIMIT 1) GROUP BY 日付;
+select * from cumulative_temp where 登熟温度 < 1300;';
+// $sql = 'SELECT date, temp FROM atmos WHERE id > :id limit 10';
 // PDOStatementクラスのインスタンスを生成します。
 $prepare = $pdo->prepare($sql);
 
